26a17473d04234a84078613891d0bb4fe08f3cef
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-04-24 20:19:26 +0300

    Various docs website improvements

--- docs/examples/FancyButton.tsx
+++ docs/examples/FancyButton.tsx
@@ -1,21 +1,26 @@
 import { Box, Text, createStyles } from '@mantine/core';
 import type { ReactNode } from 'react';
 
-const useStyles = createStyles((theme) => ({
-  root: {
-    width: '50%',
-    height: 60,
-    padding: 0,
-    border: 0,
-    borderRadius: 0,
-    cursor: 'pointer',
-    background: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0],
-    transition: 'background 0.25s ease',
-    '&:hover': {
-      background: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.fn.rgba(theme.black, 0.1),
+const useStyles = createStyles((theme) => {
+  const border = `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[4]}`;
+  return {
+    root: {
+      width: '50%',
+      height: 60,
+      padding: 0,
+      borderRadius: 0,
+      cursor: 'pointer',
+      background: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0],
+      transition: 'background 0.25s ease',
+      border: 0,
+      '&:nth-child(2n)': { borderLeft: border },
+      '&:nth-child(n+3)': { borderTop: border },
+      '&:hover': {
+        background: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.fn.rgba(theme.black, 0.1),
+      },
     },
-  },
-}));
+  };
+});
 
 export type FancyButtonProps = {
   icon: ReactNode;
