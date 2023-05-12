b5fddda968b9dee3a6138e3169a405b144be621f
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-05-12 20:17:22 +0300

    Update docs build

--- docs/next-sitemap.config.js
+++ docs/next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
+  outDir: 'out',
   siteUrl: 'https://icflorescu.github.io/mantine-contextmenu/',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
