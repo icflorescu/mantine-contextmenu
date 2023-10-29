988482e6aa5f2339c32af734d3f659a2b268e5f0
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-10-29 12:23:29 +0200

    Improve internal link component to accept hashes

--- components/InternalLink.tsx
+++ components/InternalLink.tsx
@@ -5,11 +5,14 @@ import Link from 'next/link';
 export type InternalLinkProps = React.PropsWithChildren<{
   className?: string;
   to: Route;
+  hash?: string;
 }>;
 
-export function InternalLink({ className, to, children }: InternalLinkProps) {
+export function InternalLink({ className, to, hash, children }: InternalLinkProps) {
+  let href = to;
+  if (hash) href += `#${hash}`;
   return (
-    <Anchor className={className} inherit component={Link} href={to}>
+    <Anchor className={className} inherit component={Link} href={href as Route}>
       {children}
     </Anchor>
   );
