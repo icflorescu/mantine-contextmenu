4588f6249c57acd0390bd187bdbc2c0a6a1e84b2
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-11-01 12:20:37 +0200

    Docs: SEO/webapp improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 import { ContextMenuProvider } from '__PACKAGE__';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { AppWrapper } from '~/components/AppWrapper';
 import { AUTHOR_LINK, AUTHOR_NAME, WEBSITE_LINK } from './config';
 import './layout.css';
 import classes from './layout.module.css';
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: dark)', color: '#1a1b1e' },
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+  ],
+};
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.GITHUB_PAGES === 'TRUE' ? 'https://icflorescu.github.io' : 'http://localhost:3000'),
