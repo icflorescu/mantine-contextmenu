99c5e0a0ee500d9f0d2576a7465fde263648177c
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2025-01-09 17:28:55 +0200

    Implement repositionOnRepeat

--- package/ContextMenuOverlay.tsx
+++ package/ContextMenuOverlay.tsx
@@ -1,15 +1,42 @@
+import { useContext } from 'react';
+import { ContextMenuSettingsCtx } from './ContextMenuProvider';
+
 export type ContextMenuOverlayProps = React.PropsWithChildren<{
   zIndex: number | undefined;
   onHide: () => void;
 }>;
 
 export function ContextMenuOverlay({ zIndex, children, onHide }: ContextMenuOverlayProps) {
+  const { repositionOnRepeat } = useContext(ContextMenuSettingsCtx);
+
   const handleHide = (e: React.MouseEvent) => {
     e.preventDefault();
     onHide();
   };
+
+  const handleContextMenu = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (repositionOnRepeat) {
+      const { clientX, clientY } = e;
+      try {
+        document
+          .elementsFromPoint(clientX, clientY)[1]
+          .dispatchEvent(new MouseEvent('contextmenu', { bubbles: true, clientX, clientY }));
+      } catch {
+        // ignore error
+      }
+    } else {
+      onHide();
+    }
+  };
+
   return (
-    <div className="mantine-contextmenu-overlay" style={{ zIndex }} onClick={handleHide} onContextMenu={handleHide}>
+    <div
+      className="mantine-contextmenu-overlay"
+      style={{ zIndex }}
+      onClick={handleHide}
+      onContextMenu={handleContextMenu}
+    >
       {children}
     </div>
   );
